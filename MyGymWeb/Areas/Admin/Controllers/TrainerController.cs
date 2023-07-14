7ff04316b2808fd3ad790405bb9e1e9e6efde342
ViktoR <Kachamakov@abv.bg>
2023-07-14 20:15:21 +0300

    Menage Trainers Admin Panel Ready for use

--- MyGymWeb/Areas/Admin/Controllers/TrainerController.cs
+++ MyGymWeb/Areas/Admin/Controllers/TrainerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyGymWeb.Infrastructure.Extensions;
 using MyGymWeb.Models.Home;
 using MyGymWeb.Services.Interface;
 using static MyGymWeb.Areas.Admin.AdminConstants;
@@ -17,12 +18,125 @@ namespace MyGymWeb.Areas.Admin.Controllers
 
         [HttpGet]
        
-        public async Task<IActionResult> AddTrainer()
+        public async Task<IActionResult> ManageTrainer()
         {          
                 var t = await trainerService.GetAllTrainersAsync();
                 return View(t);
             
         }
 
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new AddTrainerFormModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTrainerFormModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                await this.trainerService.AddTrainerAsync(model);
+
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError(string.Empty, "Unexpected Error");
+            }
+
+            return RedirectToAction("ManageTrainer", "Trainer", "Admin");
+
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var view = await trainerService.GetEditTrainerAsync(id);
+            return View(view);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid id, EditTrainerFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                await this.trainerService.EditByIdAsync(id, model);
+
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError(string.Empty, "Unexpected Error");
+            }
+
+            return RedirectToAction("ManageTrainer", "Trainer", "Admin");
+        }
+
+        //[HttpGet]
+
+        //public async Task<IActionResult> Delete(Guid id, TrainerDetailsRemoveViewModel model)
+        //{
+        //    string? userId = this.User.GetId();
+
+        //    if (userId == null)
+        //    {
+        //        return RedirectToAction("ManageTrainer", "Trainer", "Admin");
+        //    }
+
+        //    bool isTrainer = await this.trainerService.TrainerExistByUserId(userId);
+
+        //    if (!isTrainer)
+        //    {
+        //        return RedirectToAction("ManageTrainer", "Trainer", "Admin");
+        //    }
+
+
+        //    bool isTrue = await this.trainerService.GetTrainerUserId(id, userId);
+        //    if (isTrue)
+        //    {
+        //        var a = await trainerService.GetForDeleteAsync(id, model);
+        //        return View(a);
+        //    }
+        //    return RedirectToAction("ManageTrainer", "Trainer", "Admin");
+
+        //}
+
+       
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            //string? userId = this.User.GetId();
+
+            //if (userId == null)
+            //{
+            //    return RedirectToAction("ManageTrainer", "Trainer", "Admin");
+            //}
+
+            //bool isTrainer = await this.trainerService.TrainerExistByUserId(userId);
+
+            //if (!isTrainer)
+            //{
+            //    return RedirectToAction("ManageTrainer", "Trainer", "Admin");
+            //}
+
+
+            //bool isTrue = await this.trainerService.GetTrainerUserId(id, userId);
+           
+                await trainerService.DeleteTrainerAsync(id);
+            
+            return RedirectToAction("ManageTrainer", "Trainer", "Admin");
+        }
     }
 }
